c465097069f3cbea65164062095f244560a358b9
Saumyajit P <86034982+AgSpades@users.noreply.github.com>
2025-05-08 00:05:58 +0530

    feat: implement task creation endpoint with validation

--- main.go
+++ main.go
@@ -7,13 +7,46 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Task struct{
+	ID int `json:"id"`
+	Completed bool `json:"completed"`
+	Body string `json:"body"`
+}
+
 func main(){
 	fmt.Println("Hello again!")
 	//fmt.Println("Test")
 	app:=fiber.New()
+	
+	tasks:= []Task{}
+
 	app.Get("/", func(c *fiber.Ctx) error{
 		return c.Status(200).JSON(fiber.Map{"msg":"Hi from Task Manager"})
 	})
+
+	app.Post("/api/tasks",func(c *fiber.Ctx) error{
+		task := &Task{}
+
+		if err := c.BodyParser(task)
+		err!=nil{
+			return err
+		}
+
+		if task.Body ==""{
+			return c.Status(400).JSON(fiber.Map{
+				"error":"Task must have a description"})
+		}
+
+		task.ID= len(tasks) +1
+		tasks = append(tasks, *task)
+
+		return c.Status(201).JSON(fiber.Map{
+			"msg":"Task has been added succesfully!"})
+
+
+
+	})
+
 
 	log.Fatal(app.Listen(":4000"))
 }
