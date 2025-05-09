819bca250158ed4fd3ae49d1b0b2606e71fba50b
Saumyajit P <86034982+AgSpades@users.noreply.github.com>
2025-05-09 23:12:44 +0530

    feat: refactor task struct and main function for MongoDB integration

--- main.go
+++ main.go
@@ -1,94 +1,72 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
 type Task struct{
-	ID int `json:"id"`
+	ID int `json:"_id" bson:"_id"`
 	Completed bool `json:"completed"`
 	Body string `json:"body"`
 }
 
-func main(){
-	fmt.Println("Hello again!")
-	app:=fiber.New()
-	err:=godotenv.Load(".env")
+var collection *mongo.Collection
 
-	if err!=nil{
-		log.Fatal("Error loading .env file!")
+func main(){
+	fmt.Println("Here we go again!")
+
+	err:= godotenv.Load(".env")
+
+	if err!= nil{
+		log.Fatal("Error loading .env file",err)
 	}
 
-	PORT :=os.Getenv("PORT")
+	MONGODB_URI:=os.Getenv("MONGO_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client,err:= mongo.Connect(context.Background(),clientOptions)
 
-	tasks:= []Task{}
+	if err!=nil{
+		log.Fatal(err)
+	}
 
-	app.Get("/", func(c *fiber.Ctx) error{
-		return c.Status(200).JSON(fiber.Map{"msg":"Hi from Task Manager"})
-	})
+	err=client.Ping(context.Background(),nil)
 
-	//method for viewing list of tasks
-	app.Get("/api/tasks",func(c*fiber.Ctx) error{
-		return c.Status(200).JSON(tasks)
-	})
+	if err!=nil{
+		log.Fatal(err)
+	}
 
-	//method for adding tasks
-	app.Post("/api/tasks",func(c *fiber.Ctx) error{
-		task := &Task{}
+	log.Println("Connected to MongoDB!")
 
-		if err := c.BodyParser(task);err!=nil{
-			return err
-		}
+	collection = client.Database("tmanager_db").Collection("tasks")
 
-		if task.Body ==""{
-			return c.Status(400).JSON(fiber.Map{
-				"error":"Task must have a description"})
-		}
+	app:= fiber.New()
 
-		task.ID= len(tasks) +1
-		tasks = append(tasks, *task)
+	// endpoints for the API
 
-		return c.Status(201).JSON(task)
-
-	})
-
-	//method for marking tasks done
-	app.Patch("api/tasks/:id",func(c*fiber.Ctx) error{
-		id:=c.Params("id")
-
-		for i, task:=range tasks{
-			if fmt.Sprint(task.ID)==id{
-				tasks[i].Completed= true
-				return c.Status(200).JSON(tasks[i])
-			}
-
-		}
-
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
-	})
-
-	//method for deleting a task
-	app.Delete("api/tasks/:id",func(c *fiber.Ctx) error{
-		id:=c.Params("id")
-
-		for i, task:=range tasks{
-			if fmt.Sprint(task.ID)==id{
-				tasks= append(tasks[:i], tasks[i+1:]... )
-				
-				return c.Status(200).JSON((fiber.Map{"success":"Deleted task succesfully"}))
-			}
-
-		}
+	app.Get("/api/tasks", getTasks)
+	app.Post("/api/tasks", createTask)
+	app.Patch("/api/tasks/:id", updateTask)
+	app.Delete("/api/tasks/:id", deleteTask)
 
 
-		return c.Status(404).JSON((fiber.Map{"error":"Todo with valid id not found"}))
-	})
+	PORT := os.Getenv("PORT")
 
-
+	if PORT==""{
+		PORT="5000"
+	}
 	log.Fatal(app.Listen(":"+PORT))
+
+
 }
+
+func getTasks(c *fiber.Ctx) error {}
+func createTask(c *fiber.Ctx) error {}
+func updateTask(c *fiber.Ctx) error {}
+func deleteTask(c *fiber.Ctx) error {}
