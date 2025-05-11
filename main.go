f601d0e254595b7a28510795a339e445e53b4dc9
Saumyajit P <86034982+AgSpades@users.noreply.github.com>
2025-05-11 11:17:44 +0530

    feat: update Task struct ID field to use omitempty for JSON and BSON serialization

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 type Task struct{
-	ID primitive.ObjectID `json:"_id" bson:"_id"`
+	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Completed bool `json:"completed"`
 	Body string `json:"body"`
 }
