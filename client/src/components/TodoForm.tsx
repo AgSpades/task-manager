2f783958c4755d558ea2a66f9529a587bfb522e3
Saumyajit P <86034982+AgSpades@users.noreply.github.com>
2025-05-13 21:30:14 +0530

    feat: refactor TodoForm to use createTask mutation for task creation and update state management

--- client/src/components/TodoForm.tsx
+++ client/src/components/TodoForm.tsx
@@ -1,23 +1,51 @@
+import { BASE_URL } from "@/App";
 import { Button, Flex, Input, Spinner } from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
 
 const TodoForm = () => {
-	const [newTodo, setNewTodo] = useState("");
-	const [isPending, setIsPending] = useState(false);
+	const [newTask, setNewTask] = useState("");
 
-	const createTodo = async (e: React.FormEvent) => {
-		e.preventDefault();
-		alert("Task Created");
-		//setIsPending(true);
-	};
+	const queryClient = useQueryClient();
+
+	const { mutate: createTask, isPending: isCreating } = useMutation({
+		mutationKey: ["createTask"],
+		mutationFn: async (e: React.FormEvent) => {
+			e.preventDefault();
+			try {
+				const response = await fetch(BASE_URL + "tasks", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ body: newTask }),
+				})
+				const data = await response.json();
+				if (!response.ok) {
+					throw new Error(data.message || "Something went wrong");
+				}
+				setNewTask("");
+				return data;
+			} catch (error: any) {
+				console.error("Error creating task:", error);
+			}
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["tasks"] });
+		},
+		onError: (error) => {
+			alert(error.message);
+		}
+	})
+
 	return (
-		<form onSubmit={createTodo}>
+		<form onSubmit={createTask}>
 			<Flex gap={2}>
 				<Input
 					type='text'
-					value={newTodo}
-					onChange={(e) => setNewTodo(e.target.value)}
+					value={newTask}
+					onChange={(e) => setNewTask(e.target.value)}
 					ref={(input) => input && input.focus()}
 				/>
 				<Button
@@ -27,7 +55,7 @@ const TodoForm = () => {
 						transform: "scale(.97)",
 					}}
 				>
-					{isPending ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
+					{isCreating ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
 				</Button>
 			</Flex>
 		</form>
